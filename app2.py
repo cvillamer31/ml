d40bb80529979b4a31017c8ffa8ea66f765f0eb1
Christian VIllamer <36148483+cvillamer13@users.noreply.github.com>
2024-11-15 08:59:48 +0800

    asd

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ from scipy.spatial.distance import cdist
 import cv2
 import matching_fingerprint
 app = Flask(__name__)
-CORS(app, resources={r"/compare": {"origins": "*"}})
+CORS(app, resources={{"origins": "*"}})
 
 
 # Set up MySQL connection
